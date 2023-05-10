72019e968e12e06bbdcb97543bfa4e8784429299
Taylor Holliday <wtholliday@gmail.com>
2023-05-10 13:42:24 -0700

    Create xcframework.sh

---
+++ xcframework.sh
@@ -0,0 +1,57 @@
+
+set -e
+
+rm -rf openvdb 
+rm -rf openvdb.xcframework
+
+git clone https://github.com/AcademySoftwareFoundation/openvdb.git 
+cd openvdb 
+git checkout tags/v10.0.1
+
+# macOS build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release \
+      "-DCMAKE_OSX_ARCHITECTURES=arm64;x86_64" \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
+      -DCMAKE_INSTALL_PREFIX="$(pwd)/install" \
+      ..
+make -j8 install
+cd ..
+
+# iOS build
+mkdir build_ios
+cd build_ios
+
+# from https://cmake.org/cmake/help/latest/manual/cmake-toolchains.7.html#cross-compiling-for-ios-tvos-or-watchos
+cmake -GXcode \
+      -DCMAKE_SYSTEM_NAME=iOS \
+     "-DCMAKE_OSX_ARCHITECTURES=arm64;x86_64" \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
+      -DCMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH=NO \
+      -DCMAKE_IOS_INSTALL_COMBINED=YES \
+      .. 
+
+xcodebuild -configuration Release install
+xcodebuild -configuration Release -sdk iphonesimulator install
+
+cd ..
+
+# move libraries into place
+mkdir libs
+libtool -o libs/openvdb_macos.a build/*.a
+libtool -o libs/openvdb_ios.a build_ios/build/UninstalledProducts/iphoneos/*.a
+libtool -o libs/openvdb_ios_simulator.a build_ios/build/UninstalledProducts/iphonesimulator/*.a
+
+xcodebuild -create-xcframework \
+           -library libs/openvdb_macos.a \
+           -headers build/install/include \
+           -library libs/openvdb_ios.a \
+           -headers build/install/include \
+           -library libs/openvdb_ios_simulator.a \
+           -headers build/install/include \
+           -output openvdb.xcframework
+
+mv openvdb.xcframework ..
+cd ..
+
