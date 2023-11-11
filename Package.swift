840f281db3c324c0bdb998e3201d73cf8893498c
Taylor Holliday <taylor@audulus.com>
2023-11-11 12:13:21 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "openvdb",
-            url: "https://github.com/audulus/openvdb-spm/releases/download/v1/openvdb.xcframework.zip",
-            checksum: "f99e1bf0f44760a793bbc949e0012b0d1a4558c58313a9c1c6cfb1686290bc50"
+            url: "https://github.com/audulus/openvdb-spm/releases/download/v2/openvdb.xcframework.zip",
+            checksum: "826f8eaeb500297d64c9fd6ead487d28a9036c27e4ebbc753466828f36d0b2a3"
         )
     ]
 )
