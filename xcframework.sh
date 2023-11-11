118a6df9049259f613ae5d7a355b58835a2bb116
Taylor Holliday <taylor@audulus.com>
2023-11-11 10:41:02 -0800

    Update xcframework.sh

--- xcframework.sh
+++ xcframework.sh
@@ -1,10 +1,12 @@
 
 set -euxo pipefail
 
-rm -rf openvdb 
 rm -rf openvdb.xcframework
+rm -rf install-ios install-macos install-ios-sim
 
-
+sh boost.sh
+sh c-blosc.sh
+sh openvdb.sh
 
 xcodebuild -create-xcframework \
            -library install-macos/lib/libopenvdb.11.0.0.dylib \
@@ -13,44 +15,5 @@ xcodebuild -create-xcframework \
            -headers install-ios/include/openvdb \
            -output openvdb.xcframework
 
-# iOS build
-# mkdir build_ios
-# cd build_ios
-
-# from https://cmake.org/cmake/help/latest/manual/cmake-toolchains.7.html#cross-compiling-for-ios-tvos-or-watchos
-# make -GXcode \
-#      -DCMAKE_SYSTEM_NAME=iOS \
-#     "-DCMAKE_OSX_ARCHITECTURES=arm64;x86_64" \
-#      -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
-#      -DCMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH=NO \
-#      -DCMAKE_IOS_INSTALL_COMBINED=YES \
-#      -DBUILD_SHARED_LIBS=NO \
-#      -DBUILD_STATIC_LIBS=YES \
-#      -DBoost_NO_SYSTEM_PATHS=YES \
-#      -DBOOST_INCLUDEDIR=../../boost \
-#      -DBOOST_LIBRARYDIR=../../boost/stage/lib \
-#      .. 
-# 
-# codebuild -configuration Release install
-# codebuild -configuration Release -sdk iphonesimulator install
-# 
-# d ..
-# 
-#  move libraries into place
-# kdir libs
-# ibtool -o libs/openvdb_macos.a build/*.a
-# ibtool -o libs/openvdb_ios.a build_ios/build/UninstalledProducts/iphoneos/*.a
-# ibtool -o libs/openvdb_ios_simulator.a build_ios/build/UninstalledProducts/iphonesimulator/*.a
-# 
-# codebuild -create-xcframework \
-#           -library libs/openvdb_macos.a \
-#           -headers build/install/include \
-#           -library libs/openvdb_ios.a \
-#           -headers build/install/include \
-#           -library libs/openvdb_ios_simulator.a \
-#           -headers build/install/include \
-#           -output openvdb.xcframework
-# 
-# v openvdb.xcframework ..
-# d ..
-
+zip -r openvdb.xcframework.zip openvdb.xcframework
+swift package compute-checksum openvdb.xcframework.zip
