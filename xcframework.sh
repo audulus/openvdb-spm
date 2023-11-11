930df6006b8a06eb67782aa3fbfd7606ff1072bc
Taylor Holliday <taylor@audulus.com>
2023-11-11 13:35:46 -0800

    combine libraries, except for tbb

--- xcframework.sh
+++ xcframework.sh
@@ -9,10 +9,18 @@ sh c-blosc.sh
 sh tbb.sh
 sh openvdb.sh
 
+# tbb can't be built as a static library, so we'll use 
+# a separate package
+rm -rf install-macos/include/tbb
+rm -rf install-ios/include/tbb
+
+libtool -static -o libopenvdb-macos.a install-macos/lib/libopenvdb.a install-macos/lib/libboost_iostreams.a install-macos/lib/libboost_regex.a install-macos/lib/libblosc.a
+libtool -static -o libopenvdb-ios.a install-ios/lib/libopenvdb.a install-ios/lib/libboost_iostreams.a install-ios/lib/libboost_regex.a install-ios/lib/libblosc.a
+
 xcodebuild -create-xcframework \
-           -library install-macos/lib/libopenvdb.a \
+           -library libopenvdb-macos.a \
            -headers install-macos/include \
-           -library install-ios/lib/libopenvdb.a \
+           -library libopenvdb-ios.a \
            -headers install-ios/include \
            -output openvdb.xcframework
 
