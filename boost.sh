8c1ca8bc6d97a2ea5da7472e5fb1a2ef78634fed
Taylor Holliday <taylor@audulus.com>
2023-11-10 16:52:29 -0800

    Update boost.sh

--- boost.sh
+++ boost.sh
@@ -3,7 +3,6 @@
 
 rm -rf boost
 rm -f boost_1_83_0.tar.bz2
-mkdir deps
 
 # git clone https://github.com/boostorg/boost.git
 # git submodule update --recursive --init
@@ -12,28 +11,39 @@ wget https://boostorg.jfrog.io/artifactory/main/release/1.83.0/source/boost_1_83
 tar zxf boost_1_83_0.tar.bz2
 mv boost_1_83_0 boost
 
+export prefix=`pwd`
 cd boost
-./bootstrap.sh --prefix=deps
+
+./bootstrap.sh --prefix=$prefix/macos-install
+./b2 -a -j8 --prefix=$prefix/macos-install --with-iostreams --with-regex install
+./b2 clean
+
+# Build arm64
+./bootstrap.sh --prefix=$prefix/ios-install
 
 cat << EOF >> project-config.jam
 # IOS ARM64
 using clang : iphoneos
 : xcrun clang -arch arm64 -stdlib=libc++ -std=c++20 -miphoneos-version-min=12.0 -fvisibility-inlines-hidden -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
 ;
+EOF
 
+./b2 -a -j8 --prefix=$prefix/ios-install toolset=clang-iphoneos binary-format=mach-o abi=aapcs --with-iostreams --with-regex install
+./b2 clean
+
+# Build for simulator
+./bootstrap.sh --prefix=$prefix/ios-sim-install
+
+cat << EOF >> project-config.jam
 # IOS simulator arm64 
 using clang : iphonesimulator
 : xcrun clang -arch arm64 -stdlib=libc++ -std=c++20 -miphoneos-version-min=12.0 -fvisibility-inlines-hidden -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk
 ;
 EOF
 
-./b2 -a -j8 --prefix=/Users/holliday/openvdb-spm --with-iostreams --with-regex install
-
-# Build arm64
-# ./b2 --prefix=deps -a -j4 toolset=clang-iphoneos binary-format=mach-o abi=aapcs --with-iostreams 
-
-# Build for simulator
-# ./b2 -a -j4 toolset=clang-iphonesimulator binary-format=mach-o abi=sysv link=static $lib 
+./b2 -a -j8 --prefix=$prefix/ios-sim-install toolset=clang-iphonesimulator binary-format=mach-o abi=sysv link=static --with-iostreams --with-regex install
+./b2 clean
 
 cd ..
+rm -rf boost
 rm -f boost_1_83_0.tar.bz2
