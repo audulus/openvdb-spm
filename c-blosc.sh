3818af3cc9be33b934f1ddb40440245aecda8ae8
Taylor Holliday <taylor@audulus.com>
2023-11-10 17:17:15 -0800

    Update c-blosc.sh

--- c-blosc.sh
+++ c-blosc.sh
@@ -5,9 +5,11 @@ rm -f v1.21.5.tar.gz
 wget https://github.com/Blosc/c-blosc/archive/refs/tags/v1.21.5.tar.gz
 tar zxf v1.21.5.tar.gz
 
+export prefix=`pwd`
+
 cd c-blosc-1.21.5
 
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=/Users/holliday/openvdb-spm ..
+cmake -DCMAKE_INSTALL_PREFIX=$prefix/macos-install ..
 cmake --build . --target install
