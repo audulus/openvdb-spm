d6af9a28156e5b974c7e8bf97b440fd822bb2a29
Taylor Holliday <taylor@audulus.com>
2023-11-11 10:10:00 -0800

    Update tbb.sh

--- tbb.sh
+++ tbb.sh
@@ -2,6 +2,8 @@
 rm -rf tbb
 rm -f v2021.10.0.tar.gz
 
+export root=`pwd`
+
 # see https://github.com/Homebrew/homebrew-core/blob/5401dd66d7c7091ab20dcd46efc9ae64eb2b852e/Formula/t/tbb.rb
 
 wget https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.10.0.tar.gz
@@ -11,22 +13,18 @@ mv oneTBB-2021.10.0 tbb
 cd tbb
 
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=/Users/holliday/openvdb-spm/install-macos ..
+cmake -DCMAKE_INSTALL_PREFIX=$root/install-macos ..
 make -j 12 install
 cd ..
 rm -rf build
 
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=/Users/holliday/openvdb-spm/install-ios ..
-make -j 12 compiler=clang arch=[ia32,intel64,armv7,arm64] stdlib=libc++ stdver=c++0x target=ios install
+mkdir build-ios && cd build-ios
+cmake -DCMAKE_INSTALL_PREFIX=$root/install-ios \
+      -DCMAKE_SYSTEM_NAME=iOS \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
+      ..
+make -j 12 tbb target=ios
 cd ..
-rm -rf build
-
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=/Users/holliday/openvdb-spm/install-ios-sim ..
-make -j 12 compiler=clang arch=[ia32,intel64,armv7,arm64] stdlib=libc++ stdver=c++0x target=ios-simulator install
-cd ..
-rm -rf build
 
 rm -f v2021.10.0.tar.gz
 rm -f tbb
