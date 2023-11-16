5e08dbd8386734ed19d07347b086b8b697e42cc7
Taylor Holliday <wtholliday@gmail.com>
2023-11-15 19:45:10 -0800

    cleanup

--- c-blosc.sh
+++ c-blosc.sh
@@ -4,19 +4,19 @@ rm -rf c-blosc-1.21.5 v1.21.5.tar.gz
 wget https://github.com/Blosc/c-blosc/archive/refs/tags/v1.21.5.tar.gz
 tar zxf v1.21.5.tar.gz
 
-export prefix=`pwd`
+export root=`pwd`
 
 cd c-blosc-1.21.5
 
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$prefix/install-macos ..
+cmake -DCMAKE_INSTALL_PREFIX=$root/install-macos ..
 cmake --build . --target install
 cd ..
 
 mkdir build-ios
 cd build-ios
-cmake -DCMAKE_INSTALL_PREFIX=$prefix/install-ios \
+cmake -DCMAKE_INSTALL_PREFIX=$root/install-ios \
       -DCMAKE_SYSTEM_NAME=iOS \
       -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
       -DCMAKE_TOOLCHAIN_FILE=../../ios.toolchain.cmake \
@@ -27,7 +27,7 @@ cd ..
 
 mkdir build-ios-sim
 cd build-ios-sim
-cmake -DCMAKE_INSTALL_PREFIX=$prefix/install-ios-sim \
+cmake -DCMAKE_INSTALL_PREFIX=$root/install-ios-sim \
       -DCMAKE_TOOLCHAIN_FILE=../../ios.toolchain.cmake \
       -DPLATFORM=SIMULATORARM64 \
       ..
